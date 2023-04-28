ed229a1318c145346ca240cfbbb1966399cfa5c2
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-04-28 18:02:11 -0500

    basic inputs

---
+++ wikipediagame/src/routes/SignIn/+page.svelte
@@ -0,0 +1,6 @@
+<div class="flex items-center justfy-center h-[100vh] w-screen">  
+    <div class="flex w-screen">
+        <input class="text-black h-auto" type="email" name="email"/>
+        <input class="text-black" type="pass" name="email"/>
+    </div>
+</div>
