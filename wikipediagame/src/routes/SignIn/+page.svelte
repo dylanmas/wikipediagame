d6d3de0d993e0e41b16cfae6e4c0da6709f85eb7
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-04-28 19:20:27 -0500

    fleshed out more of the signup page

--- wikipediagame/src/routes/SignIn/+page.svelte
+++ wikipediagame/src/routes/SignIn/+page.svelte
@@ -1,7 +1,7 @@
 <div
   class="h-[100vh] w-full flex flex-col p-4 gap-4 z-10 text-white absolute transition-in justify-center self-center"
 >
-  <div class=" w-screen">
+  <div class="flex flex-col w-screen">
     <h1 class="m-3 text-4xl">Sign In</h1>
     <div class="flex flex-col w-1/3 flex-stretch">
       <input
@@ -11,14 +11,12 @@
       />
       <input class="text-black rounded-lg m-3" type="pass" name="email" />
 
-      <div class="flex flex-row place-items-center justfy-center items-center">
-        <button class="m-3 text-lg bg-white text-black w-1/3 rounded-lg"
-          >Sign In</button
-        >
-        <button class="m-3 text-lg bg-white text-black w-1/3 rounded-lg"
-          >I Don't Have A Login</button
-        >
-      </div>
+      <a
+        href="/"
+        class="m-3 text-lg bg-white text-black w-1/3 rounded-lg text-center"
+        >Sign In</a
+      >
+      <button class="text-small w-1/3 hover">I Don't Have A Login</button>
     </div>
   </div>
 </div>
