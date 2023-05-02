cb7012f2eedab9a88f648cfd62d3afc22e7d56c1
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-05-02 17:34:45 -0500

    removed on mount

--- wikipediagame/src/routes/+layout.svelte
+++ wikipediagame/src/routes/+layout.svelte
@@ -1,17 +1,8 @@
 <script>
   import "../app.css";
   import { onMount } from "svelte";
-  import firebase from "./fb";
 
-  onMount(() => {
-    // firebase.auth().onAuthStateChange((user) => {
-    //   if (user) {
-    //     console.log("user " + user + " is logged in");
-    //   } else {
-    //     console.log("Not Logged In");
-    //   }
-    // });
-  });
+  onMount(() => {});
 </script>
 
 <slot />
