678678a239ca0dcb3628a6bdb86110835c74764f
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-05-05 13:41:41 -0500

    Basic profile functionality implemented
    Dylan also helped on this :)

--- wikipediagame/src/routes/profile/+page.svelte
+++ wikipediagame/src/routes/profile/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import firebase from "../fb";
-  import { getAuth, signOut } from "firebase/auth";
+  import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
   import { goto } from "$app/navigation";
   import { getFirestore, getDoc, doc } from "firebase/firestore";
   import { onMount } from "svelte";
@@ -8,11 +8,17 @@
   const auth = getAuth();
   const db = getFirestore(firebase);
   let curDocument = {};
-  let loading = true;
+
+  let firebaseAuth = getAuth();
+
+  let loaded = false;
+
+  var docSnapData = "";
 
   onMount(async () => {
-    curDocument = (await getTheDocument());
-    console.log(curDocument.data().username);
+    curDocument = await getTheDocument();
+    // console.log(await curDocument.data().username);
+    console.log(await curDocument);
   });
   function handleSignOut() {
     signOut(auth)
@@ -29,21 +35,54 @@
   }
 
   async function getTheDocument() {
+
+    onAuthStateChanged(firebaseAuth, async (user) => {
+      if (user) {
+        // userText = "User Logged In: " + user.email;
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          console.log("Document data:", docSnap.data());
+        } else {
+          // docSnap.data() will be undefined in this case
+          console.log("No such document!");
+        }
+
+        docSnapData = docSnap.data();
+        loaded = true;
+        return docSnap.data();
+      } else {
+        console.log("You aren't logged in.");
+        return null;
+      }
+    });
+
+    /*
     const docRef = doc(db, "users", auth.currentUser.uid);
     const docSnap = await getDoc(docRef);
-    return docSnap;
+    return docSnap;*/
   }
 </script>
 
+<!--
 {#await curDocument}
 
 {:then data}
+
+{:catch error}
+<p>The error was {error.message}</p>
+{/await}
+-->
+
+{#if loaded === true}
 <div class="m-3">
   <h1 class="text-5xl">Profile</h1>
 
-  <p>{data.data().username}</p>
-  <p>{data.data().email}</p>
-  <p>{data.data().uid}</p>
+  <h1>Username: {docSnapData.username}</h1>
+  <h1>UID: {docSnapData.uid}</h1>
+  <h1>Email: {docSnapData.email}</h1>
+  
   <button
   on:click={() => goto("/")}
   class="mt-3 text-lg bg-white text-black w-1/8 rounded-lg text-center p-2"
@@ -58,6 +97,4 @@ Home
     Sign Out
   </button>
 </div>
-{:catch error}
-<p>The error was {error.message}</p>
-{/await}
+{/if}
