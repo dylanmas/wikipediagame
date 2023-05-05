84ea75881007ddaeddb7ca8592a4fb7e5518073f
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-05-04 20:19:17 -0500

    flesh out profile

--- wikipediagame/src/routes/profile/+page.svelte
+++ wikipediagame/src/routes/profile/+page.svelte
@@ -2,12 +2,17 @@
   import firebase from "../fb";
   import { getAuth, signOut } from "firebase/auth";
   import { goto } from "$app/navigation";
-  import { getFirestore, getDoc } from "firebase/firestore";
+  import { getFirestore, getDoc, doc } from "firebase/firestore";
+  import { onMount } from "svelte";
 
   const auth = getAuth();
   const db = getFirestore(firebase);
-  console.log(auth);
+  let curDocument;
 
+  onMount(async () => {
+    curDocument = await getTheDocument();
+    console.log(curDocument);
+  });
   function handleSignOut() {
     signOut(auth)
       .then(() => {
@@ -21,10 +26,25 @@
         console.log(error.message);
       });
   }
+
+  async function getTheDocument() {
+    const docRef = doc(db, "users", auth.currentUser.uid);
+    const docSnap = await getDoc(docRef);
+    return docSnap;
+  }
 </script>
 
-<div>
-  <h1>Profile</h1>
+<div class="m-3">
+  <h1 class="text-5xl">Profile</h1>
 
-  <button on:click={handleSignOut}> sign out </button>
+  <p>{curDocument.username}</p>
+  <p>{curDocument.email}</p>
+  <p>{curDocument.uid}</p>
+
+  <button
+    on:click={handleSignOut}
+    class="mt-3 text-lg bg-white text-black w-1/8 rounded-lg text-center p-2"
+  >
+    Sign Out
+  </button>
 </div>
