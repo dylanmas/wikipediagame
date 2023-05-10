749f704d699d8592d9c065adfafb0a2677ac9f98
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-05-10 13:24:15 -0500

    Update input.svelte

--- src/routes/components/input.svelte
+++ src/routes/components/input.svelte
@@ -2,12 +2,16 @@
     export var placeholder;
     export var classes;
     export var id;
+    export var name;
+    export var type;
 </script>
 
 <input
     class={classes}
     placeholder={placeholder}
     id={id}
+    type={type}
+    name={name}
     />
 
 
